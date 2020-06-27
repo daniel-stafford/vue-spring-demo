c59b4cf5eaaadbe230c33dea63201d91ce98ce83
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2020-06-27 13:12:24 +0300

    create app, simple get call

---
+++ backend/src/main/java/com/stafford/backend/HomeController.java
@@ -0,0 +1,15 @@
+package com.stafford.backend;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HomeController {
+
+  @GetMapping("/")
+  public String home() {
+    return "Hello from Spring Boot";
+  }
+}
---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  mounted() {
+    fetch('http://localhost:8085/').then(response =>
+      response.text().then(data => (this.title = data)),
+    )
+  },
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+}
+</style>
